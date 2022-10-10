c398194953b1ced83106bc15b68f3afd39f61cf9
karimch21 <anonKM1999@mail.ru>
2022-10-10 15:08:55 +0300

    feat: create script slider testimonials

--- online-zoo/pages/main/js/sliderTestimonials.js
+++ online-zoo/pages/main/js/sliderTestimonials.js
@@ -1,2 +1,25 @@
 const testimonialsCards = document.querySelector('.testimonials__cards');
 const allCardsTestimonials = document.querySelectorAll('.testimonials__card');
+const testimonialsScrollbar = document.querySelector('.testimonials-scrollbar input');
+const testimonialsCard = document.querySelector('.testimonials__card');
+
+let fullWidthTestimonialsCards = testimonialsCards.scrollWidth;
+let widthCard = testimonialsCard.getBoundingClientRect().width;
+let sumWidthCard = 0;
+let gapCards = (fullWidthTestimonialsCards - (allCardsTestimonials.length * widthCard)) / 10;
+console.log(gapCards, widthCard, fullWidthTestimonialsCards)
+
+window.addEventListener('load', () => {
+
+})
+let translateX = 0;
+testimonialsScrollbar.addEventListener('input', (e) => {
+    sumWidthCard += widthCard;
+    translateX = -(testimonialsScrollbar.value * (widthCard + gapCards));
+    // if (testimonialsScrollbar.value == testimonialsScrollbar.max) {
+    //     translateX = -fullWidthTestimonialsCards;
+    // }
+
+    testimonialsCards.style.transform = `translateX(${translateX}px)`
+
+})
