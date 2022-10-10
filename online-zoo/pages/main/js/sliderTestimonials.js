9a4744624e610088e22833303b3f49423d4d0df7
karimch21 <anonKM1999@mail.ru>
2022-10-11 00:04:46 +0300

    feat: created script testimonials__card popup

--- online-zoo/pages/main/js/sliderTestimonials.js
+++ online-zoo/pages/main/js/sliderTestimonials.js
@@ -1,3 +1,4 @@
+const testimonialsCardsWrap = document.querySelector('.testimonials__cards-wrap');
 const testimonialsCards = document.querySelector('.testimonials__cards');
 const allCardsTestimonials = document.querySelectorAll('.testimonials__card');
 const testimonialsScrollbar = document.querySelector('.testimonials-scrollbar input');
@@ -7,19 +8,87 @@ let fullWidthTestimonialsCards = testimonialsCards.scrollWidth;
 let widthCard = testimonialsCard.getBoundingClientRect().width;
 let sumWidthCard = 0;
 let gapCards = (fullWidthTestimonialsCards - (allCardsTestimonials.length * widthCard)) / 10;
-console.log(gapCards, widthCard, fullWidthTestimonialsCards)
 
-window.addEventListener('load', () => {
-
-})
 let translateX = 0;
 testimonialsScrollbar.addEventListener('input', (e) => {
     sumWidthCard += widthCard;
     translateX = -(testimonialsScrollbar.value * (widthCard + gapCards));
-    // if (testimonialsScrollbar.value == testimonialsScrollbar.max) {
-    //     translateX = -fullWidthTestimonialsCards;
-    // }
 
     testimonialsCards.style.transform = `translateX(${translateX}px)`
-
 })
+
+window.addEventListener('resize', handlerChangeWidthWindow)
+
+window.addEventListener('click', handlerClickWindow);
+
+function handlerClickWindow(e) {
+    closeTestimonialsCard(e)
+}
+handlerChangeWidthWindow()
+
+function handlerChangeWidthWindow() {
+    let windowWidth = document.documentElement.clientWidth;
+    if (640 >= windowWidth) {
+        testimonialsCards.addEventListener('click', handlerClickTestimonialsBox)
+    } else {
+        console.log(77777)
+        testimonialsCards.removeEventListener('click', handlerClickTestimonialsBox)
+    }
+}
+
+function handlerClickTestimonialsBox(e) {
+    console.log(11)
+    let card = e.target.closest('.testimonials__card');
+    if (!card) return
+    card = createTestimonialsCardClone(card);
+
+    let testimonialsBoxPopup = createTestimonialsCardsBoxPopup();
+    deleteTestimonialsCardsBoxPopup(testimonialsBoxPopup)
+    openTestimonialsCard()
+    addActiveBgBody()
+
+    testimonialsBoxPopup.append(card);
+    testimonialsCardsWrap.prepend(testimonialsBoxPopup)
+}
+
+function createTestimonialsCardClone(card) {
+    card = card.cloneNode(true)
+    card.classList.add('testimonials__card-popup');
+    let btnClose = document.createElement('button');
+    btnClose.classList.add('testimonials__card-btn-close')
+    card.appendChild(btnClose)
+    return card
+}
+
+function openTestimonialsCard() {
+    testimonialsCardsWrap.classList.add('testimonials__cards-wrap--active')
+}
+
+function closeTestimonialsCard(e) {
+
+    if (!e.target.closest('.testimonials__cards-wrap') || e.target.closest('.testimonials__card-btn-close')) {
+        testimonialsCardsWrap.classList.remove('testimonials__cards-wrap--active')
+        removeActiveBgBody()
+    }
+
+}
+
+function createTestimonialsCardsBoxPopup() {
+    let testimonialsCardsBoxPopup = document.createElement('div');
+    testimonialsCardsBoxPopup.classList.add('testimonials__box-popup', 'testimonials__box-popup--active');
+    return testimonialsCardsBoxPopup;
+}
+
+function deleteTestimonialsCardsBoxPopup(testimonialsBoxPopup) {
+    testimonialsBoxPopup = document.querySelector('.testimonials__box-popup');
+    if (!testimonialsBoxPopup) return;
+    testimonialsBoxPopup.remove()
+}
+
+function addActiveBgBody() {
+    document.querySelector('.wrapper').classList.add('wrapper--active-bg');
+}
+
+function removeActiveBgBody() {
+    document.querySelector('.wrapper').classList.remove('wrapper--active-bg');
+}
