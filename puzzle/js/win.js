c31cafd2ab490614d52a765a1299f3a929bdb013
karimch21 <anonKM1999@mail.ru>
2022-10-24 23:29:00 +0300

    feat: add win block message

--- puzzle/js/win.js
+++ puzzle/js/win.js
@@ -16,7 +16,35 @@
             winMatrix.push(i);
         }
         if (oneDimensionalMatrix === JSON.stringify(winMatrix)) {
+            stopGameTimer()
+            let message = createWinMessage();
+            document.body.appendChild(message)
             alert('win')
         }
     }
+
+    function stopGameTimer() {
+        removeClassActiveFifteen()
+    }
+
+    function createWinMessage() {
+        let messageWrap = document.createElement('div');
+        let messageContent = document.createElement('div');
+        let messageTitle = document.createElement('div');
+        let messageText = document.createElement('div');
+
+        messageWrap.classList.add('message-wrap', 'message');
+        messageContent.classList.add('message__content');
+        messageTitle.classList.add('message__title');
+        messageText.classList.add('message__text');
+
+        messageTitle.textContent = 'Вы выйграли!';
+        messageText.innerHTML = `<p>Потраченное время: ${minute}:${second}</p> <p>Количество ходов: ${moveCount}</p>`
+
+        messageWrap.appendChild(messageContent)
+        messageContent.appendChild(messageTitle)
+        messageContent.appendChild(messageText)
+        return messageWrap
+    }
+
 })()
